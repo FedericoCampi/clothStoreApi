eb6f1a40e376705ae432a98eff823146cb23d9d8
Federico Campi <federicocampi95@gmail.com>
2023-06-16 21:47:40 +0100

    as

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,9 +7,6 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      tls: {
-        rejectUnauthorized: false,
-      },
     },
     debug: false,
   },
